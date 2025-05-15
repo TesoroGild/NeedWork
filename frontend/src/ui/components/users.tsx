37b5a2cee1aacad6c44d5b4ba5888d59f3189bea
Vito <gilboss85@gmail.com>
2025-05-15 18:36:12 -0400

    connectiong front and backend

---
+++ frontend/src/ui/components/users.tsx
@@ -0,0 +1,52 @@
+"use client";
+import api from "@/api/api";
+import { useEffect, useState } from "react";
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await api.get("/users");
+      console.log(response);
+      console.log(response.data);
+      setUsers(response.data);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>affichage des users test</h1>
+      <table className="table-auto">
+        <thead>
+          <tr>
+            <th>1</th>
+            <th>2</th>
+            <th>3</th>
+            <th>4</th>
+            <th>5</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user: any, i: number) => (
+            <tr key={i}>
+              <td>{user.username}</td>
+              <td>{user.first_name}</td>
+              <td>{user.last_name}</td>
+              <td>{user.email}</td>
+              <td>{user.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UsersList;
